5b9d7fe159e916328d8ccd75485a486df2ffaa0b
Anna Cunningham <ac232@hotmail.co.uk>
2021-02-15 15:44:07 +0000

    created container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import FilmsBox from './containers/FilmsBox';
 
 function App() {
   return (
     <div className="App">
+      <FilmsBox />
      
     </div>
   );
---
+++ src/containers/FilmsBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FilmsBox = () => {
+    return(
+        <div className="films-box">
+            <h1>Upcoming Film Releases for UK</h1>
+        </div>
+    )
+};
+
+
+export default FilmsBox;
